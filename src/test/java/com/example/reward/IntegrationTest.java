8e177e352e75e9779792eff3ef8ebac8412e1ba5
MichaelY27 <yqse521749@outlook.com>
2024-04-11 15:31:21 -0700

    bug fix

--- src/test/java/com/example/reward/IntegrationTest.java
+++ src/test/java/com/example/reward/IntegrationTest.java
@@ -2,6 +2,7 @@ package com.example.reward;
 
 import com.example.reward.domain.dto.CreateTransactionDTO;
 import com.example.reward.domain.dto.ResponseDTO;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
+
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -25,11 +28,45 @@ public class IntegrationTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+
+
+
+    /**
+     * Test getTransactionById method
+     * User with id 1 should exist
+     * The response should be a success message
+     */
+    @Test
+    @Order(1)
+    @Sql(scripts = "/testdata.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void testGetTransactionByIdSuccess() {
+        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/transaction/1", ResponseDTO.class);
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals("Success", responseEntity.getBody().getMessageType());
+    }
+
+    /**
+     * Test getTransactionById method
+     * User with id 10 should not exist
+     * The response should be Error message
+     * The status code should be 404
+     */
+    @Test
+    @Order(2)
+    void testGetTransactionByIdFail() {
+        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
+                .getForEntity("http://localhost:" + port + "/transaction/10", ResponseDTO.class);
+        assertEquals(404, responseEntity.getStatusCodeValue());
+        assertEquals("Error", responseEntity.getBody().getMessageType());
+    }
+
     /**
      * Test createTransaction method
      * The response should be a success message
      */
     @Test
+    @Order(5)
     void testCreateTransaction() {
         CreateTransactionDTO createTransactionDTO = new CreateTransactionDTO(1L, BigDecimal.valueOf(100), LocalDateTime.now());
         ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
@@ -39,41 +76,26 @@ public class IntegrationTest {
     }
 
     /**
-     * Test getTransactionById method
-     * User with id 1 should exist
-     * The response should be a success message
-     */
-    @Test
-    @Sql({"/testdata.sql"})
-    void testGetTransactionByIdSuccess() {
-        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
-            .getForEntity("http://localhost:" + port + "/transaction/1", ResponseDTO.class);
-        assertEquals(200, responseEntity.getStatusCodeValue());
-        assertEquals("Success", responseEntity.getBody().getMessageType());
-    }
-
-    /**
-     * Test getTransactionById method
-     * User with id 3 should not exist
-     * The response should be Error message
-     * The status code should be 404
-     */
-    @Test
-    @Sql({"/testdata.sql"})
-    void testGetTransactionByIdFail() {
-        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
-                .getForEntity("http://localhost:" + port + "/transaction/3", ResponseDTO.class);
-        assertEquals(404, responseEntity.getStatusCodeValue());
-        assertEquals("Error", responseEntity.getBody().getMessageType());
-    }
-
-    /**
      * Test getAllTransactions method
      * User with id 1 should exist
      * The response should be a success message
      */
+    /**
+     * Test getAllTransactions method
+     * User with id 5 should not exist
+     * The response should be Error message
+     * The status code should be 422
+     */
     @Test
-    @Sql({"/testdata.sql"})
+    @Order(4)
+    void testDeleteTransactionFail() {
+        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
+                .exchange("http://localhost:" + port + "/transaction/5", HttpMethod.DELETE, null, ResponseDTO.class);
+        assertEquals(422, responseEntity.getStatusCodeValue());
+        assertEquals("Error", responseEntity.getBody().getMessageType());
+    }
+    @Test
+    @Order(5)
     void testDeleteTransactionSuccess() {
         ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
                 .exchange("http://localhost:" + port + "/transaction/1", HttpMethod.DELETE, null, ResponseDTO.class);
@@ -81,20 +103,8 @@ public class IntegrationTest {
         assertEquals("Success", responseEntity.getBody().getMessageType());
     }
 
-    /**
-     * Test getAllTransactions method
-     * User with id 3 should not exist
-     * The response should be Error message
-     * The status code should be 422
-     */
-    @Test
-    @Sql({"/testdata.sql"})
-    void testDeleteTransactionFail() {
-        ResponseEntity<ResponseDTO> responseEntity = this.restTemplate
-                .exchange("http://localhost:" + port + "/transaction/3", HttpMethod.DELETE, null, ResponseDTO.class);
-        assertEquals(422, responseEntity.getStatusCodeValue());
-        assertEquals("Error", responseEntity.getBody().getMessageType());
-    }
+
+
 
 
 }
